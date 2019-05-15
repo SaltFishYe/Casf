f3f0343345aaf8bbff03c73e0a82af5a38569af7
SaltFishYe <devilvdn@qq.com>
2019-05-15 19:40:10 +0800

    Merge branch 'master' of https://github.com/SaltFishYe/Casf
    # 请输入一个提交信息以解释此合并的必要性，尤其是将一个更新后的上游分支
    # 合并到主题分支。
    #
    # 以 '#' 开头的行将被忽略，而且空提交说明将会终止提交。

--- src/main/scala/com/saltfish/analyse/MatrixCosineAnalyse.scala
+++ src/main/scala/com/saltfish/analyse/MatrixCosineAnalyse.scala
@@ -18,6 +18,12 @@ case class MatrixCosineAnalyse(sparkSession: SparkSession, var axis: String = "x
     else "x"
   }
 
+  /**
+    * 返回两两对应的指定轴侧各向量的模
+    *
+    * @param vectorMod 指定轴侧模
+    * @return Dataset[FactorMod] (vector1,vector2,mod1,mod2)
+    */
   def genFactorMod(vectorMod: Dataset[VectorMod]): Dataset[FactorMod] = {
     val factorMod: Dataset[FactorMod] = vectorMod.agg(
       collect_list(concat_ws(":", vectorMod("vector"), vectorMod("mod"))) as "vector_mod_list"
@@ -25,15 +31,15 @@ case class MatrixCosineAnalyse(sparkSession: SparkSession, var axis: String = "x
       .flatMap {
         row => {
           var modbuffer = ArrayBuffer[(String, String, Double, Double)]()
-          var vector_mod_list = row.getAs[mutable.WrappedArray[String]](0)
+          val vector_mod_list = row.getAs[mutable.WrappedArray[String]](0)
           for (i <- 0 to vector_mod_list.size - 2) {
-            var vector_mod1 = vector_mod_list(i).split(":")
-            var vector1 = vector_mod1(0)
-            var mod1 = vector_mod1(1).toDouble
+            val vector_mod1 = vector_mod_list(i).split(":")
+            val vector1 = vector_mod1(0)
+            val mod1 = vector_mod1(1).toDouble
             for (k <- i + 1 to vector_mod_list.size - 1) {
-              var vector_mod2 = vector_mod_list(k).split(":")
-              var vector2 = vector_mod2(0)
-              var mod2 = vector_mod2(1).toDouble
+              val vector_mod2 = vector_mod_list(k).split(":")
+              val vector2 = vector_mod2(0)
+              val mod2 = vector_mod2(1).toDouble
               if (vector1.compareTo(vector2) > 0) {
                 modbuffer += ((vector1, vector2, mod1, mod2))
               } else {
@@ -48,10 +54,18 @@ case class MatrixCosineAnalyse(sparkSession: SparkSession, var axis: String = "x
     factorMod
   }
 
+  /**
+    * 返回去掉量纲的矩阵元素值
+    *
+    * @param maxValue      指定轴侧向量元素的最大值
+    * @param matrixElement 元素值
+    * @param omitRadio     省略阈值，低于此值的省略词元素；小于0时不省略
+    * @return Dataset[StandardElement] (y,x,standard_value)
+    */
   def genStandardElement(maxValue: Dataset[MaxValue],
                          matrixElement: Dataset[MatrixElement],
                          omitRadio: Double = 0.02d): Dataset[StandardElement] = {
-    var temnpStandardElement = maxValue.join(matrixElement, maxValue("axis") === matrixElement(axis))
+    val temnpStandardElement = maxValue.join(matrixElement, maxValue("axis") === matrixElement(axis))
     if (omitRadio > 0.0d) {
       temnpStandardElement.where(matrixElement("value") / maxValue("max_value") > omitRadio)
     }
@@ -64,6 +78,12 @@ case class MatrixCosineAnalyse(sparkSession: SparkSession, var axis: String = "x
     standardElement
   }
 
+  /**
+    * 返回指定轴侧向量模
+    *
+    * @param standardElement 标准值化元素值
+    * @return Dataset[VectorMod] (vector,mod)
+    */
   def genVectorMod(standardElement: Dataset[StandardElement]): Dataset[VectorMod] = {
 
     val vectorMod: Dataset[VectorMod] = standardElement.groupBy(axis)
@@ -75,6 +95,13 @@ case class MatrixCosineAnalyse(sparkSession: SparkSession, var axis: String = "x
     vectorMod
   }
 
+  /**
+    * 返回两两对应的标准值化元素值
+    *
+    * @param vectorMod       指定轴侧向量模
+    * @param standardElement 标准值化的向量元素
+    * @return Dataset[FactorStandardValue] (vector1,vector2,forecast_axis,value1,value2)
+    */
   def genFactorStandardValue(vectorMod: Dataset[VectorMod],
                              standardElement: Dataset[StandardElement]): Dataset[FactorStandardValue] = {
     val factorStandardValue: Dataset[FactorStandardValue] = vectorMod.join(standardElement, vectorMod("vector") === standardElement(axis))
@@ -89,16 +116,16 @@ case class MatrixCosineAnalyse(sparkSession: SparkSession, var axis: String = "x
       .flatMap {
         row => {
           var factorBuffer = ArrayBuffer[(String, String, String, Double, Double)]()
-          var forecast_axis = row.getString(0)
-          var vector_value_list = row.getAs[mutable.WrappedArray[String]](1)
+          val forecast_axis = row.getString(0)
+          val vector_value_list = row.getAs[mutable.WrappedArray[String]](1)
           for (i <- 0 to vector_value_list.size - 2) {
-            var vector_value1 = vector_value_list(i).split(":")
-            var vector1 = vector_value1(0)
-            var value1 = vector_value1(1).toDouble
+            val vector_value1 = vector_value_list(i).split(":")
+            val vector1 = vector_value1(0)
+            val value1 = vector_value1(1).toDouble
             for (k <- i + 1 to vector_value_list.size - 1) {
-              var vector_value2 = vector_value_list(k).split(":")
-              var vector2 = vector_value2(0)
-              var value2 = vector_value2(1).toDouble
+              val vector_value2 = vector_value_list(k).split(":")
+              val vector2 = vector_value2(0)
+              val value2 = vector_value2(1).toDouble
               if (vector1.compareTo(vector2) > 0) {
                 factorBuffer += ((vector1, vector2, forecast_axis, value1, value2))
               } else {
@@ -109,12 +136,18 @@ case class MatrixCosineAnalyse(sparkSession: SparkSession, var axis: String = "x
           factorBuffer
         }
       }
-      .toDF("vector1", "vector2","forecast_axis", "value1", "value2")
+      .toDF("vector1", "vector2", "forecast_axis", "value1", "value2")
       .as[FactorStandardValue]
 
     factorStandardValue
   }
 
+  /**
+    * 返回指定轴侧向量的元素最大值
+    *
+    * @param matrixElement (y,x,value) 向量元素值
+    * @return Dataset[MaxValue] (axis, max_value)
+    */
   def genMaxValue(matrixElement: Dataset[MatrixElement]): Dataset[MaxValue] = {
     val maxValue: Dataset[MaxValue] = matrixElement.groupBy(axis)
       .agg(max("value") as "max_value")
